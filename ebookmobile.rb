ecc5e87e71ed4483462a413a3e011675e34bee4d
Dana Scheider <dana.scheider@gmail.com>
2016-07-17 13:06:54 -0700

    Get rest of API set up

--- ebookmobile.rb
+++ ebookmobile.rb
@@ -1,3 +1,16 @@
 require "sinatra"
 require "sinatra/activerecord"
 
+Dir["./models/**/*.rb"].each {|file| require file }
+
+get "/authors" do
+  [ 200, Author.all.to_json ]
+end
+
+post "/authors" do
+  params = JSON.parse request.body.read, symbolize_names: true
+
+  @author = Author.create!(params.fetch(:author))
+
+  [ 200, @author.to_json ]
+end
